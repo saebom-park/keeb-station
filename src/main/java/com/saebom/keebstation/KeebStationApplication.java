5e949d6c7c9b4776e187b088de8c9495ba3f4aba
Saebom Park <saebom.backend@gmail.com>
2025-12-26 10:03:38 +0900

    init: keeb-station 프로젝트 초기 세팅

---
+++ src/main/java/com/saebom/keebstation/KeebStationApplication.java
@@ -0,0 +1,13 @@
+package com.saebom.keebstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeebStationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeebStationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saebom/keebstation/KeebStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saebom.keebstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeebStationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
